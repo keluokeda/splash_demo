b9cbd27286ecc36978ea65757c17376b1230a1f9
余艳辉 <913918146@qq.com>
2020-12-29 16:49:21 +0800

    Initial commit

---
+++ app/src/main/java/com/ke/splash_demo/App.kt
@@ -0,0 +1,6 @@
+package com.ke.splash_demo
+
+import android.app.Application
+
+class App:Application() {
+}
---
+++ app/src/main/java/com/ke/splash_demo/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.ke.splash_demo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
